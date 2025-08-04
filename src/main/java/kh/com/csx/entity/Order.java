62565dcd73e49f68bedcaf36343ac361c15b81d7
Lok Samady <samady.lok@csx.com.kh>
2025-08-04 16:14:19 +0700

    Describe your changes

--- src/main/java/kh/com/csx/entity/Order.java
+++ src/main/java/kh/com/csx/entity/Order.java
@@ -3,39 +3,29 @@ package kh.com.csx.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
-
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Entity
 @Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    private Long quantity;
-    private LocalDateTime orderedAt;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    private Long cartId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "customer_id")
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
     private Customer customer;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "cart_menu_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id")
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
-    private Cart cartMenu;
+    private Menu menu;
 
-    @PrePersist
-    protected void onCreate() {
-        if (orderedAt == null) {
-            orderedAt = LocalDateTime.now();
-        }
-    }
 }
-
